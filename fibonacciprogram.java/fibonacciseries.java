8490f57316c39480b80ed75d2cde9206c221e632
devivaishnavi1510-boop <devi.vaishnavi1510@gmail.com>
2026-06-05 11:25:36 +0000

    fibonacciseries

---
+++ fibonacciprogram.java/fibonacciseries.java
@@ -0,0 +1,17 @@
+package fibonacciprogram.java;
+
+public class fibonacciseries {
+    public static void main(String[] args) {
+        int n = 10; // Number of terms
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
